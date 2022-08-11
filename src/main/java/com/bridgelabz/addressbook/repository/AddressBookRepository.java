c18b5d98878be3e7b9531dbd38e32cd61e946470
PavanKumar107 <gvpavangv14@gmail.com>
2022-08-11 23:19:11 +0530

    addressbook

---
+++ src/main/java/com/bridgelabz/addressbook/repository/AddressBookRepository.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.addressbook.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.bridgelabz.addressbook.model.AddressBookModel;
+
+public interface AddressBookRepository extends JpaRepository<AddressBookModel, Long>{
+
+}
